5387ee46d48363b4023c64ff8083329ca15ecb28
kristosb <kristos_b@wp.pl>
2024-06-06 21:39:40 +0200

    added mdbtq board support, working

--- app/src/main.c
+++ app/src/main.c
@@ -4,7 +4,7 @@
  */
 
 #include <zephyr/kernel.h>
-#include <zephyr/drivers/sensor.h>
+//#include <zephyr/drivers/sensor.h>
 #include <zephyr/logging/log.h>
 
 #include <app/drivers/blink.h>
@@ -20,16 +20,17 @@ int main(void)
 {
 	int ret;
 	unsigned int period_ms = BLINK_PERIOD_MS_MAX;
-	const struct device *sensor, *blink;
-	struct sensor_value last_val = { 0 }, val;
+	const struct device *blink;
+	//const struct device *sensor, *blink;
+	//struct sensor_value last_val = { 0 }, val;
 
 	printk("Zephyr Example Application %s\n", APP_VERSION_STRING);
 
-	sensor = DEVICE_DT_GET(DT_NODELABEL(example_sensor));
-	if (!device_is_ready(sensor)) {
-		LOG_ERR("Sensor not ready");
-		return 0;
-	}
+	// sensor = DEVICE_DT_GET(DT_NODELABEL(example_sensor));
+	// if (!device_is_ready(sensor)) {
+	// 	LOG_ERR("Sensor not ready");
+	// 	return 0;
+	// }
 
 	blink = DEVICE_DT_GET(DT_NODELABEL(blink_led));
 	if (!device_is_ready(blink)) {
@@ -43,34 +44,34 @@ int main(void)
 		return 0;
 	}
 
-	printk("Use the sensor to change LED blinking period\n");
-
+	//printk("Use the sensor to change LED blinking period\n");
+	blink_set_period_ms(blink, period_ms);
 	while (1) {
-		ret = sensor_sample_fetch(sensor);
-		if (ret < 0) {
-			LOG_ERR("Could not fetch sample (%d)", ret);
-			return 0;
-		}
+		// ret = sensor_sample_fetch(sensor);
+		// if (ret < 0) {
+		// 	LOG_ERR("Could not fetch sample (%d)", ret);
+		// 	return 0;
+		// }
 
-		ret = sensor_channel_get(sensor, SENSOR_CHAN_PROX, &val);
-		if (ret < 0) {
-			LOG_ERR("Could not get sample (%d)", ret);
-			return 0;
-		}
+		// ret = sensor_channel_get(sensor, SENSOR_CHAN_PROX, &val);
+		// if (ret < 0) {
+		// 	LOG_ERR("Could not get sample (%d)", ret);
+		// 	return 0;
+		// }
 
-		if ((last_val.val1 == 0) && (val.val1 == 1)) {
+		/*if ((last_val.val1 == 0) && (val.val1 == 1)) {
 			if (period_ms == 0U) {
 				period_ms = BLINK_PERIOD_MS_MAX;
 			} else {
 				period_ms -= BLINK_PERIOD_MS_STEP;
 			}
 
-			printk("Proximity detected, setting LED period to %u ms\n",
-			       period_ms);
+			// printk("Proximity detected, setting LED period to %u ms\n",
+			//        period_ms);
 			blink_set_period_ms(blink, period_ms);
-		}
+		}*/
 
-		last_val = val;
+		//last_val = val;
 
 		k_sleep(K_MSEC(100));
 	}
