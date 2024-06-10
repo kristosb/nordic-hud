76d0bb3a87b170d57d4e2b507a289d22058a18bb
kristosb <kristos_b@wp.pl>
2024-06-10 22:38:28 +0200

    drivers st7735s and blink are working

--- boards/raytec/mdbt42q_nrf52/board.cmake
+++ boards/raytec/mdbt42q_nrf52/board.cmake
@@ -4,7 +4,7 @@
 board_runner_args(jlink "--device=nrf52" "--speed=4000")
 board_runner_args(pyocd "--target=nrf52832" "--frequency=4000000")
 
-set(OPENOCD_NRF5_SUBFAMILY "nrf52")
+#set(OPENOCD_NRF5_SUBFAMILY "nrf52")
 
 include(${ZEPHYR_BASE}/boards/common/nrfjprog.board.cmake)
 include(${ZEPHYR_BASE}/boards/common/jlink.board.cmake)
